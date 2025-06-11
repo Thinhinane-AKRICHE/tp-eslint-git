f301d66635ef089c5985f577e6ddfe7b2fc2e6dd
Thinhinane_AKRICHE <174352479+Thinhinane-AKRICHE@users.noreply.github.com>
2025-06-11 17:23:46 +0200

    Ajout de GitHub Actions

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,23 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
-
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'commonjs',
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+      },
+    },
+    extends: [js.configs.recommended],
+    rules: {
+      'no-console': 'warn',
+      'indent': ['error', 2],
+      'quotes': ['error', 'single'],
+    },
+  },
 ]);
---
+++ reponse.md
@@ -0,0 +1,2 @@
+npm run lint :
+![Erreur rencontrée](C:\Users\akric\tp-eslint-git\erreur_npm_run_lint.png)
